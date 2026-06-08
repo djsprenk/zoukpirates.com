088c4e4a2883d3c2669c39ccf52f8a6171c46f8c
djsprenk <code@djsprenk.com>
2026-06-08 17:39:27 -0400

    Update logos and add hover styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
+const fs = require('fs');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets');
+
+  eleventyConfig.addShortcode('svg', (name) =>
+    fs.readFileSync(`./src/assets/${name}.svg`, 'utf8')
+  );
 
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
